3ead2c2abc7a45112d08edea808d1e9f831cc87e
Aaron Smith <aaron.smith@hertford.ox.ac.uk>
2011-06-08 12:20:48 +0100

    have actually added the new files - didn't do this before

---
+++ plot_FEM.m
@@ -0,0 +1,33 @@
+function plot_FEM(cells,FEM_elements,FEM_nodes,axis_values_FEM,chemical_to_view,...
+	linewidth_cells,linewidth_elements,view_FEM_concentration)
+
+if view_FEM_concentration
+	FEM_angle = [0 50];
+else
+	FEM_angle = [0 90];
+end
+shading_style = 'faceted';
+
+FEM_element_nodes = FEM_elements.nodes(FEM_elements.nodes(:,1)>0,:);
+
+if view_FEM_concentration
+	trisurf(FEM_element_nodes,FEM_nodes.previous_position(:,1),FEM_nodes.previous_position(:,2),...
+		FEM_nodes.concentration(:,chemical_to_view),'linewidth',linewidth_elements)
+	% 			coloraxis = caxis;
+	% 			coloraxis = [0 0.1];
+	axis off;grid off;axis(axis_values_FEM);view(FEM_angle);shading(shading_style);
+	
+else
+	
+	trisurf(FEM_element_nodes,FEM_nodes.previous_position(:,1),FEM_nodes.previous_position(:,2),...
+		zeros(length(FEM_nodes.previous_position(:,1)),1),'linewidth',linewidth_elements)
+	axis off;grid off;axis(axis_values_FEM);view(FEM_angle);shading(shading_style);
+	hold on;
+	for current_cell=1:length(cells.vertices)
+		hold on
+		patchAS(vertices.position(cells.vertices{current_cell},:),[50,200,50]/256,linewidth_cells)
+	end
+	
+end
+
+drawnow;
---
+++ set_figure_text.m
@@ -0,0 +1,16 @@
+function set_figure_text(cells,iteration,view_iteration_number,view_number_cells)
+
+no_cells = sum(~cellfun('isempty',cells.vertices));
+
+figure_text = [];
+if view_iteration_number
+	figure_text = [' Iteration ',num2str(iteration)];
+end
+
+if view_number_cells
+	figure_text = [figure_text,' Number of cells = ',int2str(no_cells),' '];
+end
+
+text(0.5,0.7,figure_text,'FontName','FixedWidth','FontSize',14,...
+	'FontWeight','bold','HorizontalAlignment','center','EdgeColor',...
+	'black','LineWidth',2)
