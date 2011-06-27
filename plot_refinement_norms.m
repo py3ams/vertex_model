6def281d5542be785191068dacabeae511feb4ec
Aaron Smith <aaron.smith@hertford.ox.ac.uk>
2011-06-27 13:28:26 +0100

    added a fourth refinement to plot_refinement_norms.m

--- plot_refinement_norms.m
+++ plot_refinement_norms.m
@@ -4,9 +4,15 @@ refinement_0 = load('Saves/refinement_0');
 refinement_1 = load('Saves/refinement_1');
 refinement_2 = load('Saves/refinement_2');
 refinement_3 = load('Saves/refinement_3');
+refinement_4 = load('Saves/refinement_4');
 
 length_vertices = length(refinement_0.vertices.position);
 length_cells = length(refinement_0.cells.vertices);
+
+assert(sum(sum(abs(refinement_0.vertices.position-refinement_1.vertices.position)))<1e-12);
+assert(sum(sum(abs(refinement_0.vertices.position-refinement_2.vertices.position)))<1e-12);
+assert(sum(sum(abs(refinement_0.vertices.position-refinement_3.vertices.position)))<1e-12);
+assert(sum(sum(abs(refinement_0.vertices.position-refinement_4.vertices.position)))<1e-12);
 
 norms(1) = sqrt(sum([refinement_0.FEM_nodes.concentration([1:length_vertices ...
     end-length_cells:end])-refinement_1.FEM_nodes.concentration([1:length_vertices ...
@@ -20,4 +26,11 @@ norms(3) = sqrt(sum([refinement_2.FEM_nodes.concentration([1:length_vertices ...
     end-length_cells:end])-refinement_3.FEM_nodes.concentration([1:length_vertices ...
     end-length_cells:end])].^2));
 
-plot(norms)
+norms(4) = sqrt(sum([refinement_3.FEM_nodes.concentration([1:length_vertices ...
+    end-length_cells:end])-refinement_4.FEM_nodes.concentration([1:length_vertices ...
+    end-length_cells:end])].^2));
+
+plot(norms,'o')
+axis([0 5 0 3e-3])
+xlabel('Number of refinements')
+ylabel('Norm of difference from previous level')
