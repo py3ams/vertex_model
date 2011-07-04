3ea2e0361a3207dad289071992b554c09ed7ec05
Aaron Smith <aaron.smith@hertford.ox.ac.uk>
2011-07-04 12:15:58 +0100

    ran some ode_comparisons

--- ode_comparison.m
+++ ode_comparison.m
@@ -8,13 +8,19 @@ simulation_name = ['iterations_',num2str(iterations_in_test_solution),...
    '_ode_solver_',num2str(ode_solver_type)];
 
 true_solution_initial_vars = load('Saves/ode_comparison/true_solution/initial_save');
-test_solution_initial_vars = load(['Saves/',simulation_name,'/initial_save']);
+test_solution_initial_vars = load(['Saves/ode_comparison/',simulation_name,'/initial_save']);
 
 assert(sum(sum(true_solution_initial_vars.vertices.position-test_solution_initial_vars.vertices.position))==0)
 
 true_solution_vars = load('Saves/ode_comparison/true_solution/final_save');
-test_solution_vars = load(['Saves/',simulation_name,'/final_save']);
+test_solution_vars = load(['Saves/ode_comparison/',simulation_name,'/final_save']);
 
+% sqrt of sum of squares of distances
 solution_error = sqrt(sum(sum((true_solution_vars.vertices.position-test_solution_vars.vertices.position).^2,2)));
 
+% mean of distances
+% solution_error = mean(sqrt(sum((true_solution_vars.vertices.position-test_solution_vars.vertices.position).^2,2)));
+
+
+disp(['iterations : ',num2str(iterations_in_test_solution),', ode_solver : ',num2str(ode_solver_type)])
 disp(['solution error : ',num2str(solution_error)])
