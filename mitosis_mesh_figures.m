a7c15fcaf4e3e937e46e4e7acadf1339df217e33
Aaron Smith <aaron.smith@hertford.ox.ac.uk>
2011-08-30 18:07:23 +0100

    added comments and tidied up mitosis_mesh_figures and used it to make new versions of these figs for thesis

--- mitosis_mesh_figures.m
+++ mitosis_mesh_figures.m
@@ -1,11 +1,21 @@
 disp('busy');clear all; close all;
 
-red1 = [255,30,30]/255;
-green1 = [50,180,50]/255;
-green2 = [50,255,50]/255;
+% red1 = [255,30,30]/255;
+% green1 = [50,180,50]/255;
+% green2 = [50,255,50]/255;
 
-figure('outerposition',[100 100 400 400],'PaperPositionMode','auto')
-axes('position',[0.05 0.05 0.9 0.9])
+red1 = 'w';
+green1 = 'w';
+
+linewidth_cells = 5;
+linewidth_elements = 2;
+
+save_figs_logical = 1;
+
+axis_values = [-0.2 2.15 -0.25 2.0];
+
+figure('outerposition',[100 100 500 500],'PaperPositionMode','auto','color','white')
+axes('position',[0.01 0.01 0.98 0.98])
 
 cos60 = cos(pi/3);
 sin60 = sin(pi/3);
@@ -65,21 +75,25 @@ for current_node_local = 1:no_cell_nodes
     end
 end
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 original_centre = mean(vertex_positions);
 FEM_vertex_positions = [vertex_positions;original_centre];
 
-patchAS(vertex_positions,red1,5)
+patchAS(vertex_positions,red1,linewidth_cells)
 % axis equal
-axis([-0.2 2.15 -0.2 2.15])
+axis(axis_values)
 % axis tight
 axis off
 
 hold on
 
 plot(cell_to_divide_centroid(1),cell_to_divide_centroid(2),'kx','linewidth',4,'markersize',20)
+
+if save_figs_logical
+   addpath('~/Documents/export_fig/')
+   export_fig(['Figs/pre_mitosis_cell.eps'],'-nocrop');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%% pre mitosis mesh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 noise = 0.2;
 
@@ -100,16 +114,16 @@ y_2 = new_node_1_position(2);
 
 FEM_elements = [1 2 7; 2 3 7; 3 4 7; 4 5 7; 5 6 7; 6 1 7];
 
-figure('outerposition',[100 100 400 400],'PaperPositionMode','auto')
-axes('position',[0.05 0.05 0.9 0.9])
+figure('outerposition',[100 100 500 500],'PaperPositionMode','auto','color','white')
+axes('position',[0.01 0.01 0.98 0.98])
 
-patch(vertex_positions(:,1),vertex_positions(:,2),red1,'linewidth',4,'FaceAlpha',0)
+patch(vertex_positions(:,1),vertex_positions(:,2),red1,'linewidth',linewidth_cells,'FaceAlpha',0)
 hold on
 
 for i = 1:length(FEM_elements)
-    patchAS(FEM_vertex_positions(FEM_elements(i,:),:),green1,1.5)
+    patchAS(FEM_vertex_positions(FEM_elements(i,:),:),green1,linewidth_elements)
 end
-axis([-0.2 2.15 -0.2 2.15])
+axis(axis_values)
 % axis equal
 axis off
 
@@ -128,31 +142,43 @@ text(FEM_vertex_positions(6,1)-0.035,FEM_vertex_positions(6,2)-0.125,'6','FontNa
 text(FEM_vertex_positions(7,1)-0.0375,FEM_vertex_positions(7,2)-0.175,'7','FontName',...
     'FixedWidth','FontSize',20)
     
+if save_figs_logical
+   export_fig(['Figs/pre_mitosis_mesh.eps'],'-nocrop');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% post mitosis cell %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 vertex_positions = [vertex_positions;x_1 y_1;x_2 y_2];
 cells = {[1 2 3 8 7],[8 4 5 6 7]};
 
-figure('outerposition',[100 100 400 400],'PaperPositionMode','auto','color','white')
-axes('position',[0.05 0.05 0.9 0.9])
+figure('outerposition',[100 100 500 500],'PaperPositionMode','auto','color','white')
+axes('position',[0.01 0.01 0.98 0.98])
 patchAS(vertex_positions(cells{1},:),red1,5)
 patchAS(vertex_positions(cells{2},:),red1,5)
 
-axis([-0.2 2.15 -0.2 2.15])
+axis(axis_values)
 axis off
+
+if save_figs_logical
+   export_fig(['Figs/post_mitosis_cell.eps'],'-nocrop');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% post mitosis mesh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 FEM_vertex_positions = [vertex_positions;mean(vertex_positions(cells{1},:));mean(vertex_positions(cells{2},:))];
 FEM_elements = [7 1 9; 1 2 9; 2 3 9; 3 8 9; 8 7 9; 8 4 10; 4 5 10; 5 6 10; 6 7 10; 7 8 10];
 
-figure('outerposition',[100 100 400 400],'PaperPositionMode','auto','color','white')
-axes('position',[0.05 0.05 0.9 0.9])
+figure('outerposition',[100 100 500 500],'PaperPositionMode','auto','color','white')
+axes('position',[0.01 0.01 0.98 0.98])
 
 hold on
-patch(vertex_positions(cells{1},1),vertex_positions(cells{1},2),red1,'linewidth',5,'FaceAlpha',0)
-patch(vertex_positions(cells{2},1),vertex_positions(cells{2},2),red1,'linewidth',5,'FaceAlpha',0)
+patch(vertex_positions(cells{1},1),vertex_positions(cells{1},2),red1,'linewidth',linewidth_cells,'FaceAlpha',0)
+patch(vertex_positions(cells{2},1),vertex_positions(cells{2},2),red1,'linewidth',linewidth_cells,'FaceAlpha',0)
 
 for i = 1:length(FEM_elements)
-    patchAS(FEM_vertex_positions(FEM_elements(i,:),:),green1,1.5)
+    patchAS(FEM_vertex_positions(FEM_elements(i,:),:),green1,linewidth_elements)
 end
-axis([-0.2 2.15 -0.2 2.15])
+axis(axis_values)
 axis off
 
 text(FEM_vertex_positions(1,1)-0.125,FEM_vertex_positions(1,2),'1','FontName',...
@@ -176,25 +202,35 @@ text(FEM_vertex_positions(9,1),FEM_vertex_positions(9,2)-0.1,'10','FontName',...
 text(FEM_vertex_positions(10,1)+0.1,FEM_vertex_positions(10,2)-0.038,'11','FontName',...
     'FixedWidth','FontSize',20)
 
+if save_figs_logical
+   export_fig(['Figs/post_mitosis_mesh.eps'],'-nocrop');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% temp mitosis mesh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 FEM_vertex_positions = [FEM_vertex_positions;original_centre];
 FEM_elements1 = [7 1 11; 1 2 11; 2 3 11; 3 8 11;];
 FEM_elements2 = [8 4 11; 4 5 11; 5 6 11; 6 7 11];
 
-figure('outerposition',[100 100 400 400],'PaperPositionMode','auto')
-axes('position',[0.05 0.05 0.9 0.9])
+figure('outerposition',[100 100 500 500],'PaperPositionMode','auto','color','white')
+axes('position',[0.01 0.01 0.98 0.98])
 hold on
-patch(vertex_positions(cells{1},1),vertex_positions(cells{1},2),red1,'linewidth',5,'FaceAlpha',0)
-patch(vertex_positions(cells{2},1),vertex_positions(cells{2},2),red1,'linewidth',5,'FaceAlpha',0)
+patch(vertex_positions(cells{1},1),vertex_positions(cells{1},2),red1,'linewidth',linewidth_cells,'FaceAlpha',0)
+patch(vertex_positions(cells{2},1),vertex_positions(cells{2},2),red1,'linewidth',linewidth_cells,'FaceAlpha',0)
 for i = 1:length(FEM_elements1)
-    patchAS(FEM_vertex_positions(FEM_elements1(i,:),:),green1,1.5)
+    patchAS(FEM_vertex_positions(FEM_elements1(i,:),:),green1,linewidth_elements)
 end
-axis([-0.2 2.15 -0.2 2.15])
+axis(axis_values)
 axis off
 
 for i = 1:length(FEM_elements2)
-    patchAS(FEM_vertex_positions(FEM_elements2(i,:),:),green2,1.5)
+    patchAS(FEM_vertex_positions(FEM_elements2(i,:),:),green1,linewidth_elements)
 end
-axis([-0.2 2.15 -0.2 2.15])
+axis(axis_values)
 axis off
+
+if save_figs_logical
+   export_fig(['Figs/temp_mitosis_cell.eps'],'-nocrop');
+end
 
 
