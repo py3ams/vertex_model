6712dd2162a0ddc3c5193e7d38f62b7ba12f30f1
Aaron Smith <aaron.smith@hertford.ox.ac.uk>
2011-08-30 14:27:25 +0100

    made a few changes in cell_figs_from_saves to enable me to replot discrete approximation mesh figs

--- cell_figs_from_saves.m
+++ cell_figs_from_saves.m
@@ -8,16 +8,18 @@ refinement_level = 1;
 save_figs_logical = 0;
 initial_fig_logical = 1;
 
-folder_name = 'refinement_comparison_movie';
-saved_iterations = [100 1000];
+folder_name = 'realtime_refinement_comparison/true_iterations_8000_refinements_4';
+saved_iterations = [1000];
 
 % we set these outside function so both cell_fig and fem_fig have access to them
-temp_axis_values = 0.65*[-1 1 -1 1];
+temp_axis_values = 0.55*[-1 1 -1 1];
 % temp_axis_values = [-0.1 0.1 -0.07 0.13];
 % apoptosis_figs
 % temp_axis_values = [0.115 0.215 0.195 0.295];
 % proliferation_figs
 % temp_axis_values = [-0.125 0.025 -0.015 0.115];
+% mesh figs (from realtime_refinement_comparison/true_iterations_8000_refinements_4)
+% temp_axis_values = [-0.55 0.55 -0.52 0.58];
 
 % temp_axis_values = [0.05 0.55 -0.25 0.25];
 
@@ -40,9 +42,9 @@ if initial_fig_logical
     
     if FEM_logical
     
-       % FEM_element_nodes = FEM_elements{refinement_level}.nodes;
-       % FEM_node_positions = FEM_nodes{refinement_level}.position;
-       % FEM_node_concentrations = FEM_nodes{refinement_level}.concentration;
+%        FEM_element_nodes = FEM_elements{refinement_level}.nodes;
+%        FEM_node_positions = FEM_nodes{refinement_level}.position;
+%        FEM_node_concentrations = FEM_nodes{refinement_level}.concentration;
        
        FEM_element_nodes = FEM_elements.nodes;
        FEM_node_positions = FEM_nodes.position;
@@ -79,9 +81,9 @@ for unused_variable = 1:length(saved_iterations)
         
     if FEM_logical
     
-       %     FEM_element_nodes = FEM_elements{refinement_level}.nodes;
-       %     FEM_node_positions = FEM_nodes{refinement_level}.position;
-       %     FEM_node_concentrations = FEM_nodes{refinement_level}.concentration;
+%            FEM_element_nodes = FEM_elements{refinement_level}.nodes;
+%            FEM_node_positions = FEM_nodes{refinement_level}.position;
+%            FEM_node_concentrations = FEM_nodes{refinement_level}.concentration;
        
        FEM_element_nodes = FEM_elements.nodes;
        FEM_node_positions = FEM_nodes.position;
@@ -101,7 +103,7 @@ end
 % function more versatile
 function cell_fig(cell_vertices,vertex_positions,fig_name,folder_name,save_figs_logical,temp_axis_values)
 
-linewidth = 6;
+linewidth = 5;
 
 figure('position',[100 100 500 500],'color','white','PaperPositionMode','auto')
 axes('position',[0.01 0.01 0.98 0.98],'linewidth',2,'xcolor','white','ycolor','w',...
@@ -127,46 +129,52 @@ end
 function fem_fig(FEM_element_nodes,FEM_node_positions,FEM_node_concentrations,...
     cell_vertices,vertex_positions,fig_name,folder_name,save_figs_logical,temp_axis_values)
 
-cell_concentration_logical = true;
-Dpp_view = [0 60];
-linewidth = 1;
+cell_concentration_logical = false;
+if cell_concentration_logical 
+    Dpp_view = [0 60];
+else
+    Dpp_view = [0 90];
+end
+linewidth_cells = 5;
+linewidth_elements = 2;
 temp_axis_values_FEM = [temp_axis_values 0 0.1];
 caxis_vals = [0 0.1];
 green1 = [50,180,50]/255;
-white = [255,255,0]/255;
-colormap_val = [linspace(green1(1),white(1),300)' ...
-    linspace(green1(2),white(2),300)' linspace(green1(3),white(3),300)'];
+white1 = [255,255,0]/255;
+colormap_val = [linspace(green1(1),white1(1),300)' ...
+    linspace(green1(2),white1(2),300)' linspace(green1(3),white1(3),300)'];
 shading_style = 'faceted';
 
 figure('position',[100 100 500 500],'color','white','PaperPositionMode','auto')
 axes('position',[0.01 0.01 0.98 0.98],'linewidth',2,'xcolor','white','ycolor','w',...
-   'zcolor','w','ticklength',[0 0],'xtick',[],'ytick',[])
+    'zcolor','w','ticklength',[0 0],'xtick',[],'ytick',[])
 
 FEM_element_nodes = FEM_element_nodes(FEM_element_nodes(:,1)>0,:);
 
 if ~cell_concentration_logical
-   
-   trisurf(FEM_element_nodes,FEM_node_positions(:,1),FEM_node_positions(:,2),...
-      zeros(length(FEM_node_positions(:,1)),1),'linewidth',linewidth)
-   
-   grid off;axis off;shading(shading_style);axis(temp_axis_values);
-   caxis(caxis_vals);colormap(colormap_val);view(Dpp_view);
-   
-   hold on
-   cellfun(@(x)patch(vertex_positions(x,1),vertex_positions(x,2),'k','linewidth',3,'FaceAlpha',0),cell_vertices);
-   
+    
+    trisurf(FEM_element_nodes,FEM_node_positions(:,1),FEM_node_positions(:,2),...
+        zeros(length(FEM_node_positions(:,1)),1),'linewidth',linewidth_elements)
+    
+    grid off;axis off;shading(shading_style);caxis(caxis_vals); view(Dpp_view);colormap(colormap_val)
+    
+    axis(temp_axis_values);
+    
+    cellfun(@(x)patch(vertex_positions(x,1),vertex_positions(x,2),'w','linewidth',linewidth_cells,'FaceAlpha',0),cell_vertices);
+    
 else
-   
-   trisurf(FEM_element_nodes,FEM_node_positions(:,1),FEM_node_positions(:,2),...
-      FEM_node_concentrations,'linewidth',linewidth)
-   
-   grid off;axis off;shading(shading_style);axis(temp_axis_values_FEM);
-   caxis(caxis_vals);colormap(colormap_val);view(Dpp_view);
+    
+    trisurf(FEM_element_nodes,FEM_node_positions(:,1),FEM_node_positions(:,2),...
+        FEM_node_concentrations,'linewidth',linewidth_elements)
+    
+    grid off;axis off;shading(shading_style);axis(temp_axis_values_FEM);
+    caxis(caxis_vals);colormap(colormap_val);view(Dpp_view);
    
 end
 
 if save_figs_logical
-   saveas(gcf,['Figs/',folder_name,fig_name,'.eps'],'psc2')
+   addpath('~/Documents/export_fig/')
+   export_fig(['Figs/',folder_name,'/',folder_name,'_',fig_name,'.eps'],'-nocrop');
 end
 % close;
 
