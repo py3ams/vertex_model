2cd75fad50078bea25839fbf62f56f0be9b2eb7c
Aaron Smith <aaron.smith@hertford.ox.ac.uk>
2011-10-06 11:37:59 +0100

    edited error_function_ingestion to divide by the total time for better comparison between different simulations

--- error_function_ingestion.m
+++ error_function_ingestion.m
@@ -1,6 +1,6 @@
-% disp('busy');close all; clear all;
-% 
-% load Saves/cellular_production_and_ingestion/final_save
+disp('busy');close all; clear all;
+
+load Saves/cellular_production_and_ingestion/final_save
 
 statistics_counter = stats.counter;
 total_concentration = stats.total_concentration(1:statistics_counter,:);
@@ -9,6 +9,6 @@ total_internal_chemical = stats.internal_chemical_quantity(1:statistics_counter,
 total_dead_chemical = stats.dead_chemical(1:statistics_counter);
 net_chemical = total_chemical_released-total_internal_chemical-total_dead_chemical;
 
-error_function = sqrt(delta_t*sum((total_concentration-net_chemical).^2));
+error_function = sqrt(1/total_time*delta_t*sum((1-net_chemical./total_concentration).^2));
 
 disp(['Error : ',num2str(error_function)])
